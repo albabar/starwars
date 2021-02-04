e0857d2846ede2b5de73cc1120a1f18ced07b1c8
Babar Al-Amin <knock@babar.im>
2021-02-04 11:45:30 +0100

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <div className="App">
+          <header className="App-header">
+            <Link to="/">Home
+              <img src={logo} className="App-logo" alt="logo" />
+            </Link>
+            <Link to="/movies">Movies</Link>
+            <p>
+              Edit <code>src/App.js</code> and save to reload.
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+              Learn React
+            </a>
+          </header>
+          <Switch>
+            <Route path="/movies">
+              <div>All the movies</div>
+            </Route>
+            <Route path="/">
+              <div>Testing my heart.</div>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
